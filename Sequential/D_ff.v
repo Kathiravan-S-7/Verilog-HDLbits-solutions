c9380a59e1a96d886cfbfa482523d4372873c05e
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-01-22 21:49:12 +0530

    Create D_ff.v

---
+++ Sequential/D_ff.v
@@ -0,0 +1,12 @@
+// D Flip-Flop
+// Author: Kathiravan S
+
+module top_module (
+    input  clk,
+    input  d,
+    output reg q
+);
+    always @(posedge clk) begin
+        q <= d;
+    end
+endmodule
