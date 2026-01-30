e728ff6018f36468995523d59579345d738370af
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-01-30 21:08:00 +0530

    Create population_count_255bit.v

---
+++ combinational/population_count_255bit.v
@@ -0,0 +1,17 @@
+// Description: 255-bit Population Count Circuit Counts number of '1's in input vector
+// Author: Kathiravan S
+module top_module( 
+    input  [254:0] in,
+    output reg [7:0] out
+);
+
+    integer i;
+
+    always @(*) begin
+        out = 0;
+        for (i = 0; i < 255; i = i + 1) begin
+            out = out + in[i];
+        end
+    end
+
+endmodule
