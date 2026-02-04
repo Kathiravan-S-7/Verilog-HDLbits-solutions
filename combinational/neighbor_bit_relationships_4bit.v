2f3f854da3ca9db221a14eb806084a5edd1471dc
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-02-04 23:04:03 +0530

    Create neighbor_bit_relationships_4bit.v

---
+++ combinational/neighbor_bit_relationships_4bit.v
@@ -0,0 +1,15 @@
+// 4-bit neighbour bit relationships: both, any, and different (combinational logic)
+// Author: Kathiravan S
+
+module top_module( 
+    input  [3:0] in,
+    output [2:0] out_both,
+    output [3:1] out_any,
+    output [3:0] out_different
+);
+
+    assign out_both      = in[2:0] & in[3:1];
+    assign out_any       = in[3:1] | in[2:0];
+    assign out_different = in ^ {in[0], in[3:1]};
+
+endmodule
