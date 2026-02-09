71615edadbe1f6b3d8675148d67b2e27c5720bb3
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-02-09 20:39:40 +0530

    Create mux_kmap.v

---
+++ combinational/mux_kmap.v
@@ -0,0 +1,16 @@
+// Logic implementation using 4:1 MUX inputs derived from K-map
+// Author: Kathiravan S
+
+module top_module (
+    input c,
+    input d,
+    output [3:0] mux_in
+);
+
+    assign mux_in[0] = c | d;
+    assign mux_in[1] = 1'b0;
+    assign mux_in[2] = ~d;
+    assign mux_in[3] = c & d;
+
+endmodule
+
