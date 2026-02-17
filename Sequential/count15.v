d3db61ad5bb4fa9402037b031dda256173d756e4
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-02-17 20:55:39 +0530

    Create count15.v

---
+++ Sequential/count15.v
@@ -0,0 +1,19 @@
+// 4-bit synchronous mod-16 binary counter
+// Author: Kathiravan S
+
+module top_module (
+    input clk,
+    input reset,         
+    output reg [3:0] q
+);
+
+    always @(posedge clk) begin
+        if (reset)
+            q <= 4'b0000;
+        else
+            q <= q + 1'b1;
+    end
+
+endmodule
+
+
