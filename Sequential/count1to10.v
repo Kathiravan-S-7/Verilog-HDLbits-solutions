4cee5b8b7978f6f8d08161bcc5889bad4a167b4c
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-02-18 19:57:18 +0530

    Create count1to10.v

---
+++ Sequential/count1to10.v
@@ -0,0 +1,20 @@
+// Synchronous decade counter counting from 1 to 10
+// Author: Kathiravan S
+
+
+module top_module (
+    input clk,
+    input reset,          // Synchronous active-high reset
+    output reg [3:0] q
+);
+
+    always @(posedge clk) begin
+        if (reset)
+            q <= 4'd1;
+        else if (q == 4'd10)
+            q <= 4'd1;
+        else
+            q <= q + 1'b1;
+    end
+
+endmodule
