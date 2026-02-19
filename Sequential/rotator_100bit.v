d9857a1adb981bf5def34ca3d2e3eb93803e11cd
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-02-19 21:21:28 +0530

    Create rotator_100bit.v

---
+++ Sequential/rotator_100bit.v
@@ -0,0 +1,23 @@
+// 100-bit left/right rotator with synchronous load 
+// Author: Kathiravan S
+
+module top_module(
+    input clk,
+    input load,
+    input [1:0] ena,
+    input [99:0] data,
+    output reg [99:0] q
+);
+
+    always @(posedge clk) begin
+        if (load)
+            q <= data;                                   
+        else if (ena == 2'b01)
+            q <= {q[0], q[99:1]};                        
+        else if (ena == 2'b10)
+            q <= {q[98:0], q[99]};                       
+        // else: do nothing (hold value)
+    end
+
+endmodule
+
