6d4a867bfb6d75cd4c9a685a140bd7f95d50b63a
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-02-23 17:35:05 +0530

    Create rule90_512.v

---
+++ Sequential/rule90_512.v
@@ -0,0 +1,20 @@
+// 512-bit Rule 90 cellular automaton with synchronous load and zero boundary conditions
+// Author: Kathiravan S
+
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output reg [511:0] q ); 
+
+    always @(posedge clk) begin
+        if (load)
+            q <= data;
+        else begin
+            q[0]     <= q[1];                
+            q[511]   <= q[510];              
+            q[510:1] <= q[511:2] ^ q[509:0]; 
+        end
+    end
+
+endmodule
