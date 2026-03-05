020753530336d6a6b91b1646d252b7cee4029f91
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-03-05 22:05:13 +0530

    Create lemmings2_fsm.v

---
+++ Sequential/lemmings2_fsm.v
@@ -0,0 +1,69 @@
+// Moore FSM implementation of Lemmings with falling behavior
+// Author: Kathiravan S
+
+module top_module(
+    input clk,
+    input areset,
+    input bump_left,
+    input bump_right,
+    input ground,
+    output walk_left,
+    output walk_right,
+    output aaah );
+
+parameter LEFT=0, RIGHT=1, FALL_L=2, FALL_R=3;
+
+reg [1:0] state, next_state;
+
+always @(*) begin
+    case(state)
+
+        LEFT: begin
+            if (!ground)
+                next_state = FALL_L;
+            else if (bump_left)
+                next_state = RIGHT;
+            else
+                next_state = LEFT;
+        end
+
+        RIGHT: begin
+            if (!ground)
+                next_state = FALL_R;
+            else if (bump_right)
+                next_state = LEFT;
+            else
+                next_state = RIGHT;
+        end
+
+        FALL_L: begin
+            if (ground)
+                next_state = LEFT;
+            else
+                next_state = FALL_L;
+        end
+
+        FALL_R: begin
+            if (ground)
+                next_state = RIGHT;
+            else
+                next_state = FALL_R;
+        end
+
+    endcase
+end
+
+
+always @(posedge clk or posedge areset) begin
+    if (areset)
+        state <= LEFT;
+    else
+        state <= next_state;
+end
+
+
+assign walk_left  = (state == LEFT);
+assign walk_right = (state == RIGHT);
+assign aaah = (state == FALL_L) || (state == FALL_R);
+
+endmodule
