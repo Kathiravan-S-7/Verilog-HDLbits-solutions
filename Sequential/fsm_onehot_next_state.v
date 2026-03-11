6aaaf8e4fa81b2693ba87418635d052b9e5bb642
Kathiravan-S-7 <kathiravan.profo.in@gmail.com>
2026-03-11 19:55:22 +0530

    Create fsm_onehot_next_state.v

---
+++ Sequential/fsm_onehot_next_state.v
@@ -0,0 +1,18 @@
+// One-hot FSM next-state logic for states B and D
+// Author: Kathiravan S
+
+
+module top_module (
+    input [6:1] y,
+    input w,
+    output Y2,
+    output Y4 );
+
+assign Y2 = y[1] & ~w;
+
+assign Y4 = (y[2] & w) |
+            (y[3] & w) |
+            (y[5] & w) |
+            (y[6] & ~w);
+
+endmodule
